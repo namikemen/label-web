7a48166ebab50e4bdac9c8620dd7e6016c93d602
namikemen <namdt2132@icloud.com>
2023-11-16 14:03:25 +0700

    simple tool v0.1

---
+++ json2yolo.py
@@ -0,0 +1,14 @@
+import json
+
+# Load the data from the JSON file
+with open('labels.json') as f:
+    data = json.load(f)
+
+# For each key-value pair in the data
+for filename, labels in data.items():
+    # Create a new text file with the key as the filename
+    with open(f'yolo_label/{filename}.txt', 'w') as f:
+        # For each label in the list
+        for label in labels:
+            # Write the label to the file, followed by a newline
+            f.write(' '.join(map(str, label)) + '\n')
